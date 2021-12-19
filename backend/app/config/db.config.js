a5f009b27917454f2139d4829eea68d47d9beaa3
unknown <jimlibo13@gmail.com>
2021-12-19 22:59:13 +0200

    Credentials are hidden now

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {   // configuration for our db
 	HOST: "localhost",     
 	USER: "****",       // fill with your own username
-	PASSWORD: "******",  // fill with your own password
+	PASSWORD: "*****",  // fill with your own password
 	DB: " "       // fill with the name of the database 
 };
