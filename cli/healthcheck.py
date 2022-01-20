208569a230bc9bd86aa383630a8b2820f80f91ae
unknown <jimlibo13@gmail.com>
2022-01-20 13:33:54 +0200

    added cli functionality for healthcheck

---
+++ cli/healthcheck.py
@@ -0,0 +1,20 @@
+#! usr/bin/env python3
+
+import argparse   # for command-line arguments
+import requests   # for communicating with the api
+import json       # for json format
+
+
+def healthcheck(ar):
+    res = requests.get('http://localhost:9103')
+    print(res.status_code)   # printing returning code
+    print(res.json())       # printing  returned message
+
+    return True
+
+
+# right now, parser has no arguments, although this will probably change
+parser = argparse.ArgumentParser()   # creates a parser
+args = parser.parse_args()          # gets the values of parser's arguments
+
+healthcheck(args)
