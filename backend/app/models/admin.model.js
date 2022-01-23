c6e9c66791c437456c7ebf19651c68fd8fd8fad6
unknown <jimlibo13@gmail.com>
2022-01-23 20:05:55 +0200

    fixed the returning string to match the db credentials

--- backend/app/models/admin.model.js
+++ backend/app/models/admin.model.js
@@ -1,4 +1,5 @@
 const sql = require("./db.js");
+const conf = require("../config/db.config.js");   // for the credentials
 
 const Admin = function(admin) {
 	this.username = admin.username;
@@ -8,12 +9,13 @@ const Admin = function(admin) {
 
 Admin.check_connection = result => {   // check connection with the database
 	sql.ping((err) => {
+		connection_string = "mysql://" + conf.USER + ":" + conf.PASSWORD + "@" + conf.HOST + "/" + conf.DB;   // string with connection details
 		if (err) {   // connection with the database is closed
-			result(err, {connection_details: "mysql://username:password@hostname/database"});
+			result(err, {connection_details: connection_string});
 			return;
 		} 
 		// connection with the database is open
-		result(null, {connection_details: "mysql://username:password@hostname/database"});
+		result(null, {connection_details: connection_string});
 		return;
 	});
 };
