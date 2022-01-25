7b0f7d540386f2b9781b7e4ae9f7c073911bfbca
unknown <jimlibo13@gmail.com>
2022-01-26 00:09:47 +0200

    added admin authrization

--- backend/app/controllers/admin.controller.js
+++ backend/app/controllers/admin.controller.js
@@ -14,3 +14,21 @@ exports.healthcheck = (req, res) => {
 		});
 	});
 };
+
+exports.isAdmin = (req, res, next) => {    // function to check whether someone is admin or not
+	Admin.exists(req.params.username, req.params.password, (err, data) => {
+		if (err) {   // if error occurred
+			if (err.kind === 'not_found') {   // wrong credentials
+				res.status(400).send({
+					message: "Not authorized"
+				});
+			} else {   // other (interval) error
+				res.status(500).send({
+					message: "Internal Server Error"
+				});
+			}
+		} else {   // no errors, correct credentials
+			next();
+		}
+	});
+};
